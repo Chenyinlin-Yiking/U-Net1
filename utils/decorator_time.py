a7ff16c95b9ac9a6e96f726d2318d5d7e0fd10e2
StudyofSlag <75869957+Chenyinlin-Yiking@users.noreply.github.com>
2023-05-25 15:24:43 +0800

    Update decorator_time.py
    修改了注释

--- utils/decorator_time.py
+++ utils/decorator_time.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author  : Jinzhong Xu
-# @Contact : jinzhongxu@csu.ac.cn
-# @Time    : 2020/11/11 10:06
-# @File    : decorator_time.py
-# @Software: PyCharm
-
 import time
 import functools
 
 
 def display_time(text):
+    
     """
     装饰器，用来计算并输出函数运行时间
     :param text: 个性化输出语句
